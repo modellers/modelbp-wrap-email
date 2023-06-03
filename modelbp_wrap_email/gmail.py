b311ea51d7f61b659d1348a4047ba29261eb6de7
Kjartan Jónsson <kjartan@vedur.is>
2023-06-03 11:12:42 +0000

    Add new gmail code

--- modelbp_wrap_email/gmail.py
+++ modelbp_wrap_email/gmail.py
@@ -1,69 +1,84 @@
-"""
-Sends email using Gmail API
-
-More information
-https://mailtrap.io/blog/python-send-email-gmail/ (section: How to send an email with Python via Gmail API?)
-"""
-
+import os
+import pickle
 import base64
-from email.mime.text import MIMEText
+from google.auth.transport.requests import Request  # pip install google-auth-oauthlib
 from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
-from requests import HTTPError
-import os, sys
-import argparse
+from googleapiclient.discovery import build     # pip install google-api-python-client
+from googleapiclient.errors import HttpError
+from google.oauth2.credentials import Credentials   # pip install google-auth
+
+# Constants
+SCOPES = ['https://www.googleapis.com/auth/gmail.send']
+CREDENTIALS_FILE = '../secrets/client_secret_196341740635-p5j8hmhopb89qhotitjj83ecsidbhr1s.apps.googleusercontent.com.json'
+TOKEN_FILE = 'token.pickle'
 
 def get_auth():
-    SCOPES = [
-            "https://www.googleapis.com/auth/gmail.send"
-        ]
-    # get credentials from file in secrets folder if it exists
-    if os.path.exists('../secrets/gmail_credential.json'):
-        flow = InstalledAppFlow.from_client_secrets_file('../secrets/gmail_credential.json', SCOPES)
-        creds = flow.run_local_server(port=0)
-    else:
-        # get credentials from environment variables if they exist
-        config = {
-            "installed": {
-                "client_id": os.environ.get("GMAIL_CLIENT_ID"),
-                "project_id": os.environ.get("GMAIL_PROJECT_ID"),
-                "auth_uri": os.environ.get("GMAIL_AUTH_URI"),
-                "token_uri": os.environ.get("GMAIL_TOKEN_URI"),
-                "auth_provider_x509_cert_url": os.environ.get("GMAIL_AUTH_PROVIDER_X509_CERT_URL"),
-                "client_secret": os.environ.get("GMAIL_CLIENT_SECRET"),
-                "redirect_uris": os.environ.get("GMAIL_REDIRECT_URIS")
-            }
-        }
-        # check if credentials are valid
-        if config["installed"]["client_id"] is None:
-            print("No credentials found. Expected to find them in secrets/gmail_credential.json or environment variables.")
-            sys.exit(1)
-        # create flow
-        flow = InstalledAppFlow.from_client_config(config, SCOPES)
-        # get credentials
-        creds = flow.run_local_server(port=0)
-
+    creds = None
+    if os.path.exists(TOKEN_FILE):
+        with open(TOKEN_FILE, 'rb') as token:
+            creds = pickle.load(token)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                CREDENTIALS_FILE, SCOPES)
+            creds = flow.run_local_server(port=0)
+        with open(TOKEN_FILE, 'wb') as token:
+            pickle.dump(creds, token)
     return creds
 
-def sendText(message, to_email, credentials):
-    service = build('gmail', 'v1', credentials=credentials)
-    message = MIMEText('This is the body of the email')
-    message['to'] = to_email
-    message['subject'] = 'Test'
-    create_message = {'raw': base64.urlsafe_b64encode(message.as_bytes()).decode()}
+def create_message(sender, to, subject, message_text):
+    message = {
+        'raw': base64.urlsafe_b64encode(
+            f"From: {sender}\r\n"
+            f"To: {to}\r\n"
+            f"Subject: {subject}\r\n\r\n"
+            f"{message_text}".encode("utf-8")
+        ).decode("utf-8")
+    }
+    return message
+
+def send_message(service, user_id, message):
     try:
-        message = (service.users().messages().send(userId="me", body=create_message).execute())
-        print(F'sent message to {message} Message Id: {message["id"]}')
-    except HTTPError as error:
-        print(F'An error occurred: {error}')
-        message = None
+        message = service.users().messages().send(
+            userId=user_id, body=message).execute()
+        print("Message sent successfully!")
+        return message
+    except HttpError as error:
+        print(f"An error occurred: {error}")
+
+
+def send_email(sender, to, subject, message_text, creds=None):
+    if creds is None:
+        creds = get_auth()
+
+    # Create Gmail service
+    service = build('gmail', 'v1', credentials=creds)
+
+    message = create_message(sender, to, subject, message_text)
+
+    # Send the email
+    send_message(service, 'me', message)
+
+
+def main():
+    # Authenticate
+    creds = get_auth()
+
+    # Create Gmail service
+    service = build('gmail', 'v1', credentials=creds)
+
+    # Compose the email
+    sender = 'rob@papersread.org'
+    to = 'kjartan.papersread@gmail.com'
+    subject = 'Test Email'
+    message_text = 'Hello, this is a test email sent using the Gmail API SDK.'
+
+    message = create_message(sender, to, subject, message_text)
+
+    # Send the email
+    send_message(service, 'me', message)
 
 if __name__ == '__main__':
-    credentials = get_auth()
-    # using arg parser to get arguments
-    parser = argparse.ArgumentParser(description='Send email using Gmail API')
-    parser.add_argument('-m', '--message', help='Message to send', required=False)
-    parser.add_argument('-t', '--to', help='Email address to send to', required=False)
-    args = parser.parse_args()
-    if args.message and args.to:
-        sendText(message=args.message, to_email=args.to, credentials=credentials)
+    main()
